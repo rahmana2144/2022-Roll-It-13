6d2734bc2a38d36ff4ef189381d49fd25d0b69b2
rahmana2144 <159388318+rahmana2144@users.noreply.github.com>
2024-03-01 10:01:01 +1300

    stats

---
+++ Stats_v2.py
@@ -0,0 +1,34 @@
+# finds the lowest, highest and average score from list
+def get_stats(stats_list):
+    pass
+
+    # sort the lists.
+    user_scores.sort()
+    comp_scores.sort()
+
+    # find the lowest, highest and average scores...
+    user_low = user_scores[0]
+    user_high = user_scores[-1]
+    user_average = sum(user_scores) / len(user_scores)
+
+
+# create lists to hold user and computer scores
+user_scores = []
+comp_scores = []
+# Loop six times - for testing purposes. ask the user to enter the
+# score for the user and the computer for each round
+# for item in range(0, 6):
+#     user_score = int(input("Enter the user score:"))
+#     comp_score = int(input("Enter the computer score"))
+#
+#     # add user score to list of user scores!
+#     user_scores.append(user_score)
+#     comp_scores.append(comp_score)
+
+# calculate the lowest, highest and average
+# scores and display them.
+
+
+print("Low: ", user_low)
+print("High: ", user_high)
+print("Average", user_average)
